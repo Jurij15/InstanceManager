dddf2bde24406b4c4c541f41e9b317076ab33275
Jurij15 <jurij.grom1@gmail.com>
2022-10-11 21:37:54 +0200

    added version

---
+++ Version.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstanceManager
+{
+    public static class Version
+    {
+        public static double VersionNumber = 0.1;
+
+        public static string VersionString = VersionNumber.ToString();
+    }
+}
